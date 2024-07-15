ab28b0f382ce9f1882ee8db9b15dca9bb5c6bc5d
황병현 <grant0808@naver.com>
2024-07-15 13:28:53 +0900

    ⭐feat : member model

--- backend/Signal/src/main/java/com/ssafy/signal/member/domain/Member.java
+++ backend/Signal/src/main/java/com/ssafy/signal/member/domain/Member.java
@@ -1,4 +1,44 @@
 package com.ssafy.signal.member.domain;
 
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Entity
+@NoArgsConstructor
+@Getter
+@Builder
+@AllArgsConstructor
 public class Member {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "user_id", nullable = false)
+    private String userId;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+
+    @Column(name = "type")
+    private int type;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "REFRESH_TOKEN")
+    private String refreshToken;
+
+    // refreshToken 재설정
+    public void updateRefreshToken(String refreshToken) {
+        this.refreshToken = refreshToken;
+    }
+
+    // 비밀번호 암호화
+    public void passwordEncode(PasswordEncoder passwordEncoder) {
+        this.password = passwordEncoder.encode(this.password);
+    }
+
 }
