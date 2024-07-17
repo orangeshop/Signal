227db52d0e54da16423ac96c75f74047cbea13ed
최영호 <rhkeh123@naver.com>
2024-07-17 17:01:36 +0900

    feat : room db 데이터 적용

---
+++ android/Signal/app/src/main/java/com/ongo/signal/data/repository/main/chat/ChatHomeDao.kt
@@ -0,0 +1,19 @@
+package com.ongo.signal.data.repository.main.chat
+
+import androidx.lifecycle.MutableLiveData
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.ongo.signal.data.model.chat.ChatHomeChildDto
+import com.ongo.signal.data.model.chat.ChatHomeDTO
+
+
+@Dao
+interface ChatHomeDao {
+    @Query("SELECT * FROM chat_home_room")
+    suspend fun getAll(): List<ChatHomeDTO>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertAll(vararg chatHomeDTOs: ChatHomeDTO)
+}
