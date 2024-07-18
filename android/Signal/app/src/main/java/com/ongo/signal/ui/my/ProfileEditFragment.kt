d50fa0a6e3a001f351c04dd0c1f16bdff93907bb
upsk1 <55921031+upsk1@users.noreply.github.com>
2024-07-18 15:27:53 +0900

    feat: ProfileEditFragment 추가

---
+++ android/Signal/app/src/main/java/com/ongo/signal/ui/my/ProfileEditFragment.kt
@@ -0,0 +1,14 @@
+package com.ongo.signal.ui.my
+
+import com.ongo.signal.R
+import com.ongo.signal.config.BaseFragment
+import com.ongo.signal.databinding.FragmentProfileEditBinding
+
+
+class ProfileEditFragment :
+    BaseFragment<FragmentProfileEditBinding>(R.layout.fragment_profile_edit) {
+    override fun init() {
+        
+    }
+
+}
