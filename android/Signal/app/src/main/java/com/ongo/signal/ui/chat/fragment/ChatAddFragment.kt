c0ea4b20401fff97a5ad62b9da22bdf6edf510ea
최영호 <rhkeh123@naver.com>
2024-07-18 10:20:31 +0900

    feat : 화면 이동 로직 추가

---
+++ android/Signal/app/src/main/java/com/ongo/signal/ui/chat/fragment/ChatAddFragment.kt
@@ -0,0 +1,13 @@
+package com.ongo.signal.ui.chat.fragment
+
+import com.ongo.signal.R
+import com.ongo.signal.config.BaseFragment
+import com.ongo.signal.databinding.FragmentChatBinding
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class ChatAddFragment : BaseFragment<FragmentChatBinding>(R.layout.fragment_chat_add) {
+    override fun init() {
+
+    }
+}
