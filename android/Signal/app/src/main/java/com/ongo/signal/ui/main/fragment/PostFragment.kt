8c21ffd16428bcedb86c5b9afe994bf50f4db93d
DESKTOP-6ABJ64L\SSAFY <cjh0630@naver.com>
2024-07-18 10:54:46 +0900

    feat: Chip Adapter 작성

--- android/Signal/app/src/main/java/com/ongo/signal/ui/main/fragment/PostFragment.kt
+++ android/Signal/app/src/main/java/com/ongo/signal/ui/main/fragment/PostFragment.kt
@@ -9,6 +9,7 @@ import com.ongo.signal.data.model.main.CommentDTO
 import com.ongo.signal.databinding.FragmentPostBinding
 import com.ongo.signal.ui.main.adapter.CommentAdapter
 import com.ongo.signal.ui.main.MainViewModel
+import com.ongo.signal.ui.main.adapter.ChipAdapter
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.launch
@@ -17,27 +18,30 @@ import kotlinx.coroutines.launch
 class PostFragment : BaseFragment<FragmentPostBinding>(R.layout.fragment_post) {
 
     private lateinit var commentAdapter: CommentAdapter
+    private lateinit var chipAdapter: ChipAdapter
     private val viewModel: MainViewModel by activityViewModels()
 
     override fun init() {
         commentAdapter = CommentAdapter()
+        chipAdapter = ChipAdapter()
+
         binding.rvComment.apply {
             layoutManager = LinearLayoutManager(requireContext())
             adapter = commentAdapter
+        }
+
+        binding.rvChip.apply {
+            layoutManager =
+                LinearLayoutManager(requireContext(), LinearLayoutManager.HORIZONTAL, false)
+            adapter = chipAdapter
         }
 
         lifecycleScope.launch {
             viewModel.selectedPost.collectLatest { post ->
                 post?.let {
                     binding.post = it
-
-                    val comments = listOf(
-                        CommentDTO(commentId = "1", content = "This is a comment.", userId = "user1", userName = "User One"),
-                        CommentDTO(commentId = "2", content = "This is another comment.", userId = "user2", userName = "User Two"),
-                        CommentDTO(commentId = "3", content = "Yet another comment.", userId = "user3", userName = "User Three")
-                    )
-
-                    commentAdapter.submitList(comments)
+                    chipAdapter.submitList(it.tags)
+                    commentAdapter.submitList(it.comment)
                 }
             }
         }
