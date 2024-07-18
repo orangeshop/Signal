866e68a53c21ac997691234a6db23139d002c650
DESKTOP-6ABJ64L\SSAFY <cjh0630@naver.com>
2024-07-18 17:29:19 +0900

    feat: 뒤로가기 기능 추가

--- android/Signal/app/src/main/java/com/ongo/signal/ui/MainActivity.kt
+++ android/Signal/app/src/main/java/com/ongo/signal/ui/MainActivity.kt
@@ -1,21 +1,23 @@
 package com.ongo.signal.ui
 
-import android.os.Build
-import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
-import androidx.annotation.RequiresApi
+import androidx.activity.OnBackPressedCallback
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import androidx.core.view.WindowInsetsControllerCompat
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
 import com.ongo.signal.R
 import com.ongo.signal.config.BaseActivity
 import com.ongo.signal.databinding.ActivityMainBinding
+import com.ongo.signal.ui.chat.ChatFragment
+import com.ongo.signal.ui.main.fragment.MainFragment
+import com.ongo.signal.ui.match.MatchFragment
+import com.ongo.signal.ui.my.MyPageFragment
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
+
+    private lateinit var navHostFragment: NavHostFragment
 
     override fun setupBinding(binding: ActivityMainBinding) {
 
@@ -25,12 +27,27 @@ class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
             insets
         }
 
-        val navHostFragment = supportFragmentManager
+        navHostFragment = supportFragmentManager
             .findFragmentById(R.id.nav_host_fragment) as NavHostFragment
         val navController = navHostFragment.navController
 
         binding.bottomNavigation.setupWithNavController(navController)
+
+        onBackPressedDispatcher.addCallback(this, object : OnBackPressedCallback(true) {
+            override fun handleOnBackPressed() {
+                handleBackPressed()
+            }
+        })
     }
 
+    private fun handleBackPressed() {
+        val currentFragment = navHostFragment.childFragmentManager.primaryNavigationFragment
+        val navController = navHostFragment.navController
+        if (currentFragment is MainFragment || currentFragment is MatchFragment || currentFragment is ChatFragment || currentFragment is MyPageFragment) {
+            finish()
+        } else {
+            navController.popBackStack()
+        }
+    }
 
 }
