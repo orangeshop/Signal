bf45298653797ba06f76da17952fdc25a7886f9f
upsk1 <55921031+upsk1@users.noreply.github.com>
2024-07-19 10:58:20 +0900

    feat: 미션 애니메이션 구현

--- android/Signal/app/src/main/java/com/ongo/signal/ui/Match/MatchFragment.kt
+++ android/Signal/app/src/main/java/com/ongo/signal/ui/Match/MatchFragment.kt
@@ -1,13 +1,20 @@
 package com.ongo.signal.ui.match
 
+import android.view.View
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
 import com.ongo.signal.R
 import com.ongo.signal.config.BaseFragment
 import com.ongo.signal.databinding.FragmentMatchBinding
 
 class MatchFragment : BaseFragment<FragmentMatchBinding>(R.layout.fragment_match) {
     override fun init() {
-        binding.apply {
+        initAnimation()
+    }
 
-        }
+    private fun initAnimation(){
+        val anim = AnimationUtils.loadAnimation(requireContext(),R.anim.anim_alpha)
+        val matchView = requireView().findViewById<View>(R.id.cl_match)
+        matchView.startAnimation(anim)
     }
 }
