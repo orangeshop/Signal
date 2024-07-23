940ea24bd8ce5c8ba18e781d66a35cc4d12eeafb
김민조 <upsk1@naver.com>
2024-07-23 22:20:55 +0900

    fix: 사라진 mainactivity 소스 복구

--- android/Signal/app/src/main/java/com/ongo/signal/ui/MainActivity.kt
+++ android/Signal/app/src/main/java/com/ongo/signal/ui/MainActivity.kt
@@ -1,16 +1,19 @@
 package com.ongo.signal.ui
 
 
-import android.os.Bundle
-import android.view.View.GONE
-import android.view.View.VISIBLE
-import androidx.activity.enableEdgeToEdge
-import androidx.activity.OnBackPressedCallback
+import android.Manifest
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.os.Build
 import android.view.View
+import androidx.activity.OnBackPressedCallback
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.firebase.messaging.FirebaseMessaging
 import com.ongo.signal.R
 import com.ongo.signal.config.BaseActivity
 import com.ongo.signal.databinding.ActivityMainBinding
@@ -18,12 +21,19 @@ import com.ongo.signal.ui.chat.fragment.ChatFragment
 import com.ongo.signal.ui.main.fragment.MainFragment
 import com.ongo.signal.ui.match.MatchFragment
 import com.ongo.signal.ui.my.MyPageFragment
+import com.ssafy.firebase_b.util.PermissionChecker
 import dagger.hilt.android.AndroidEntryPoint
+import timber.log.Timber
 
 @AndroidEntryPoint
 class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
 
     private lateinit var navHostFragment: NavHostFragment
+    private val checker = PermissionChecker(this)
+    private val runtimePermissions = arrayOf(
+        Manifest.permission.POST_NOTIFICATIONS,
+        Manifest.permission.CAMERA,
+    )
 
     override fun setupBinding(binding: ActivityMainBinding) {
 
@@ -54,7 +64,49 @@ class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
                 else -> hideBottomNavigation()
             }
         }
+
+        checkPermission()
     }
+
+    private fun checkPermission() {
+        if (!checker.checkPermission(this, runtimePermissions)) {
+            checker.setOnGrantedListener {
+                initFCM()
+            }
+            checker.requestPermissionLauncher.launch(runtimePermissions)
+        } else {
+            initFCM()
+        }
+    }
+
+    private fun initFCM() {
+        getFCMToken()
+        createNotificationChannel(CHANNEL_ID, "ongo")
+    }
+
+    private fun getFCMToken() {
+        FirebaseMessaging.getInstance().token.addOnCompleteListener(OnCompleteListener { task ->
+            if (!task.isSuccessful) {
+                Timber.d("FCM 토큰 얻기에 실패")
+                return@OnCompleteListener
+            }
+            Timber.d("token 정보: ${task.result ?: "task.result is null"}")
+            if (task.result != null) {
+                uploadToken(task.result)
+            }
+        })
+    }
+
+    private fun createNotificationChannel(id: String, name: String) {
+        val importance = NotificationManager.IMPORTANCE_HIGH
+        val notificationManager: NotificationManager =
+            getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            notificationManager.createNotificationChannel(NotificationChannel(id, name, importance))
+        }
+    }
+
 
     private fun handleBackPressed() {
         val currentFragment = navHostFragment.childFragmentManager.primaryNavigationFragment
@@ -70,7 +122,7 @@ class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
         binding.bottomNavigation.visibility = View.GONE
     }
 
-    fun showBottomNavigation(){
+    fun showBottomNavigation() {
         binding.bottomNavigation.visibility = View.VISIBLE
     }
 
