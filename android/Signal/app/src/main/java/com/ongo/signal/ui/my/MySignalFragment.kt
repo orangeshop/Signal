780bd12a4fdb1f4d6dabfd21b7266f480c3555aa
upsk1 <55921031+upsk1@users.noreply.github.com>
2024-07-23 10:10:19 +0900

    feat: MySignal 프래그먼트 생성

---
+++ android/Signal/app/src/main/java/com/ongo/signal/ui/my/MySignalFragment.kt
@@ -0,0 +1,11 @@
+package com.ongo.signal.ui.my
+
+import com.ongo.signal.R
+import com.ongo.signal.config.BaseFragment
+import com.ongo.signal.databinding.FragmentMySignalBinding
+
+class MySignalFragment : BaseFragment<FragmentMySignalBinding>(R.layout.fragment_my_signal) {
+    override fun init() {
+        
+    }
+}
