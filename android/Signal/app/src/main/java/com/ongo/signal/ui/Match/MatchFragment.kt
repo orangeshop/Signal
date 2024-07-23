bef5a1e660d595d9c8e226cfe98aef1002bc1c3e
김민조 <upsk1@naver.com>
2024-07-23 23:05:28 +0900

    feat: 위경도, 위치 거리 구하는 함수 구현

--- android/Signal/app/src/main/java/com/ongo/signal/ui/Match/MatchFragment.kt
+++ android/Signal/app/src/main/java/com/ongo/signal/ui/Match/MatchFragment.kt
@@ -1,13 +1,25 @@
 package com.ongo.signal.ui.match
 
 import android.Manifest
+import android.annotation.SuppressLint
+import android.location.Address
+import android.location.Geocoder
+import android.location.Location
 import android.view.View
 import android.view.animation.AnimationUtils
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.location.Priority
 import com.ongo.signal.R
 import com.ongo.signal.config.BaseFragment
 import com.ongo.signal.databinding.FragmentMatchBinding
 import com.ongo.signal.util.RadarView
 import com.ssafy.firebase_b.util.PermissionChecker
+import timber.log.Timber
+import java.util.Locale
+import kotlin.math.atan2
+import kotlin.math.cos
+import kotlin.math.sin
+import kotlin.math.sqrt
 
 class MatchFragment : BaseFragment<FragmentMatchBinding>(R.layout.fragment_match) {
 
@@ -22,15 +34,8 @@ class MatchFragment : BaseFragment<FragmentMatchBinding>(R.layout.fragment_match
     override fun init() {
         initViews()
         initAnimation()
-        checkPermission()
     }
 
-    private fun checkPermission() {
-        if (!checker.checkPermission(requireContext(), runtimePermissions)) {
-            checker.setOnGrantedListener {}
-            checker.requestPermissionLauncher.launch(runtimePermissions)
-        }
-    }
 
     private fun initAnimation() {
         val anim = AnimationUtils.loadAnimation(requireContext(), R.anim.anim_alpha)
@@ -42,11 +47,62 @@ class MatchFragment : BaseFragment<FragmentMatchBinding>(R.layout.fragment_match
         radarView = binding.rvRadar
 
         binding.btnComplete.setOnClickListener {
+
+            if (!checker.checkPermission(requireContext(), runtimePermissions)) {
+                checker.setOnGrantedListener {}
+                checker.requestPermissionLauncher.launch(runtimePermissions)
+                makeToast("권한을 허락해주셔야 매칭을 진행할 수 있습니다.")
+                return@setOnClickListener
+            }
+
             hideRequestMatchingWidget()
             showRadarWidget()
+            getLocation()
         }
 
     }
+
+
+    @SuppressLint("MissingPermission")
+    private fun getLocation() {
+
+        val fusedLocationProviderClient =
+            LocationServices.getFusedLocationProviderClient(requireActivity())
+
+        fusedLocationProviderClient.getCurrentLocation(Priority.PRIORITY_HIGH_ACCURACY, null)
+            .addOnSuccessListener { success: Location? ->
+                success?.let { location ->
+                    Timber.d("${location.latitude}, ${location.longitude}")
+                    val geocoder = Geocoder(requireContext(), Locale.KOREA)
+                    val address = geocoder.getFromLocation(
+                        location.latitude,
+                        location.longitude,
+                        1
+                    ) as List<Address>
+                    Timber.d("위치는 : $address")
+                    Timber.d("거리는 : ${calculateDistance(36.1020544,128.4213672,36.1020544,127.4213672)}")
+                }
+            }
+            .addOnFailureListener { fail ->
+                Timber.d("fail : $fail")
+            }
+    }
+
+    private fun calculateDistance(lat1: Double, lon1: Double, lat2: Double, lon2: Double): Double {
+        val earthRadius = 6371.0
+
+        val dLat = Math.toRadians(lat2 - lat1)
+        val dLon = Math.toRadians(lon2 - lon1)
+
+        val a = sin(dLat / 2) * sin(dLat / 2) +
+                cos(Math.toRadians(lat1)) * cos(Math.toRadians(lat2)) *
+                sin(dLon / 2) * sin(dLon / 2)
+
+        val c = 2 * atan2(sqrt(a), sqrt(1 - a))
+
+        return earthRadius * c
+    }
+
 
     private fun hideRequestMatchingWidget() {
         with(binding) {
