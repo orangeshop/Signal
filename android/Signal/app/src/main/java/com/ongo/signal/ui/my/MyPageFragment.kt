1c973a8cdfbb12ae7e97cf8b15951c79cafc45b5
upsk1 <55921031+upsk1@users.noreply.github.com>
2024-07-23 14:07:07 +0900

    feat: 로그아웃 클릭 이벤트 추가

--- android/Signal/app/src/main/java/com/ongo/signal/ui/my/MyPageFragment.kt
+++ android/Signal/app/src/main/java/com/ongo/signal/ui/my/MyPageFragment.kt
@@ -1,10 +1,12 @@
 package com.ongo.signal.ui.my
 
+import android.content.Intent
 import androidx.fragment.app.commit
 import androidx.navigation.fragment.findNavController
 import com.ongo.signal.R
 import com.ongo.signal.config.BaseFragment
 import com.ongo.signal.databinding.FragmentMypageBinding
+import com.ongo.signal.ui.LoginActivity
 import com.ongo.signal.ui.MainActivity
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -21,6 +23,13 @@ class MyPageFragment : BaseFragment<FragmentMypageBinding>(R.layout.fragment_myp
         binding.tvEditProfile.setOnClickListener {
             goToProfileEdit()
         }
+
+        binding.tvLogout.setOnClickListener {
+            goToLoginActivity()
+        }
+        binding.tvLogout.setOnClickListener {
+            goToLoginActivity()
+        }
     }
 
     private fun goToProfileEdit() {
@@ -28,5 +37,11 @@ class MyPageFragment : BaseFragment<FragmentMypageBinding>(R.layout.fragment_myp
             (requireActivity() as MainActivity).hideBottomNavigation()
             findNavController().navigate(R.id.action_myPageFragment_to_profileEditFragment)
         }
+    }
+
+    private fun goToLoginActivity() {
+        val intent = Intent(requireContext(), LoginActivity::class.java)
+        startActivity(intent)
+        requireActivity().finish()
     }
 }
