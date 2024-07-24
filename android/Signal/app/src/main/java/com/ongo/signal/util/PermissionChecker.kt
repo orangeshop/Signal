02226f1ad40e172427e5c09a39dec0c7c1bb8027
DESKTOP-6ABJ64L\SSAFY <cjh0630@naver.com>
2024-07-24 14:31:15 +0900

    chore: 주석 제거

--- android/Signal/app/src/main/java/com/ongo/signal/util/PermissionChecker.kt
+++ android/Signal/app/src/main/java/com/ongo/signal/util/PermissionChecker.kt
@@ -1,18 +1,16 @@
-package com.ssafy.firebase_b.util
+package com.ongo.signal.util
 
 import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.net.Uri
 import android.provider.Settings
-import android.widget.Toast
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.fragment.app.Fragment
-import com.ongo.signal.util.OnGrantedListener
 import timber.log.Timber
 
 
