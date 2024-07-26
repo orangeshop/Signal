9ac1f8fdfdb7d6578b1c351cff77af81a7ef3554
DESKTOP-6ABJ64L\SSAFY <cjh0630@naver.com>
2024-07-26 17:15:13 +0900

    feat: 경로 추가

--- android/Signal/app/src/main/java/com/ongo/signal/data/model/main/CommentDTO.kt
+++
@@ -1,9 +0,0 @@
-package com.ongo.signal.data.model.main
-
-data class CommentDTO(
-    val commentId: String,
-    val content: String,
-    val userId: String = "",
-    val userName: String = "도라에몽",
-    val userProfile: String = ""
-)
