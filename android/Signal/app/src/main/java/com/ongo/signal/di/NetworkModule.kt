ee0e59e6fa49a3beaeb392963811f0d82399490f
DESKTOP-6ABJ64L\SSAFY <cjh0630@naver.com>
2024-07-26 17:23:11 +0900

    feat: baseURL 변경, 서비스 등록

--- android/Signal/app/src/main/java/com/ongo/signal/di/NetworkModule.kt
+++ android/Signal/app/src/main/java/com/ongo/signal/di/NetworkModule.kt
@@ -2,12 +2,14 @@ package com.ongo.signal.di
 
 import com.google.gson.Gson
 import com.google.gson.GsonBuilder
+import com.ongo.signal.network.MainApi
 import com.ongo.signal.network.SignalApi
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import java.util.concurrent.TimeUnit
@@ -25,17 +27,22 @@ object NetworkModule {
         .setLenient()
         .create()
 
+    private val loggingInterceptor = HttpLoggingInterceptor().apply {
+        level = HttpLoggingInterceptor.Level.BODY
+    }
+
     @Singleton
     @Provides
     fun provideOkHttpClient(): OkHttpClient = OkHttpClient.Builder()
         .readTimeout(5000, TimeUnit.MILLISECONDS)
         .connectTimeout(5000, TimeUnit.MILLISECONDS)
+        .addInterceptor(loggingInterceptor)
         .build()
 
     @Singleton
     @Provides
     fun provideSignalRetrofit(gson: Gson): Retrofit = Retrofit.Builder()
-        .baseUrl("baseurl")
+        .baseUrl("http://192.168.100.200:8080/")
         .addConverterFactory(GsonConverterFactory.create(gson))
         .build()
 
@@ -43,4 +50,9 @@ object NetworkModule {
     @Provides
     fun provideGptApiService(retrofit: Retrofit): SignalApi =
         retrofit.create(SignalApi::class.java)
+
+    @Singleton
+    @Provides
+    fun provideMainApiService(retrofit: Retrofit): MainApi =
+        retrofit.create(MainApi::class.java)
 }
