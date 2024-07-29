4a88904f5caafc3e13bb28ab08d9f132e125ecd1
upsk1 <55921031+upsk1@users.noreply.github.com>
2024-07-29 13:40:39 +0900

    feat: 유저 세션 추가

--- android/Signal/app/src/main/java/com/ongo/signal/ui/login/LoginFragment.kt
+++ android/Signal/app/src/main/java/com/ongo/signal/ui/login/LoginFragment.kt
@@ -5,11 +5,13 @@ import androidx.fragment.app.commit
 import androidx.fragment.app.viewModels
 import com.ongo.signal.R
 import com.ongo.signal.config.BaseFragment
+import com.ongo.signal.config.UserSession
 import com.ongo.signal.data.model.login.LoginRequest
 import com.ongo.signal.databinding.FragmentLoginBinding
 import com.ongo.signal.ui.MainActivity
 import dagger.hilt.android.AndroidEntryPoint
 import timber.log.Timber
+import kotlin.math.sign
 
 @AndroidEntryPoint
 class LoginFragment : BaseFragment<FragmentLoginBinding>(R.layout.fragment_login) {
@@ -41,6 +43,12 @@ class LoginFragment : BaseFragment<FragmentLoginBinding>(R.layout.fragment_login
                 ),
                 onSuccess = { signalUser ->
                     Timber.d("로그인 성공 $signalUser")
+                    // TODO refactor 구조 ..
+                    UserSession.userId = signalUser.userId
+                    UserSession.userName = signalUser.userName
+                    UserSession.accessToken = signalUser.accessToken
+
+                    
                     val intent = Intent(requireContext(), MainActivity::class.java)
                     startActivity(intent)
                     requireActivity().finish()
