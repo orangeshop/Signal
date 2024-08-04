036087ebcbb6515cf303778156709f41225a3df8
choi-jihun <cjh0630@naver.com>
2024-08-05 02:46:47 +0900

    feat: page를 통한 게시글 불러오기

--- android/Signal/app/src/main/java/com/ongo/signal/data/repository/main/board/BoardRepositoryImpl.kt
+++ android/Signal/app/src/main/java/com/ongo/signal/data/repository/main/board/BoardRepositoryImpl.kt
@@ -17,22 +17,22 @@ class BoardRepositoryImpl @Inject constructor(private val mainApi: MainApi) : Bo
         return mainApi.writeBoard(boardRequestDTO)
     }
 
-    override suspend fun readBoard(): Response<List<BoardDTO>> {
-        return mainApi.readBoard()
+    override suspend fun readBoard(pageNum: Int, limit: Int): Response<List<BoardDTO>> {
+        return mainApi.readBoard(pageNum, limit)
     }
 
-    override suspend fun readBoardById(boardId: Int): Response<BoardDTO> {
+    override suspend fun readBoardById(boardId: Long): Response<BoardDTO> {
         return mainApi.readBoardById(boardId)
     }
 
     override suspend fun updateBoard(
-        boardId: Int,
+        boardId: Long,
         updateBoardDTO: UpdateBoardDTO
     ): Response<BoardDTO> {
         return mainApi.updateBoard(boardId, updateBoardDTO)
     }
 
-    override suspend fun deleteBoard(boardId: Int): Response<ResponseBody> {
+    override suspend fun deleteBoard(boardId: Long): Response<ResponseBody> {
         return mainApi.deleteBoard(boardId)
     }
 
