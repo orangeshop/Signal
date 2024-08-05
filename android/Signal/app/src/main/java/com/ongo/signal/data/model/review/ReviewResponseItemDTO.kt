5ee2d3b94f4356cbbe68fc6ff36a89247e55dec2
DESKTOP-6ABJ64L\SSAFY <cjh0630@naver.com>
2024-08-05 17:51:17 +0900

    feat: 리뷰에 활용할 DTO 추가

---
+++ android/Signal/app/src/main/java/com/ongo/signal/data/model/review/ReviewResponseItemDTO.kt
@@ -0,0 +1,11 @@
+package com.ongo.signal.data.model.review
+
+import com.google.gson.annotations.SerializedName
+
+data class ReviewResponseItemDTO(
+    @SerializedName("review_id") val reviewId: Long,
+    @SerializedName("user_id") val userId: Long,
+    @SerializedName("content") val content: String,
+    @SerializedName("writer_id") val writerId: Long,
+    @SerializedName("star") val star: Int
+)
