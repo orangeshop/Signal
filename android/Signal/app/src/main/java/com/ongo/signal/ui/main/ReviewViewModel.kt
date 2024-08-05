293d5c097df6a267adfbbd8ae3aeb5341f8bc2f0
DESKTOP-6ABJ64L\SSAFY <cjh0630@naver.com>
2024-08-05 17:38:59 +0900

    feat: 리뷰 작성 및 불러오기 구현

--- android/Signal/app/src/main/java/com/ongo/signal/ui/main/ReviewViewModel.kt
+++ android/Signal/app/src/main/java/com/ongo/signal/ui/main/ReviewViewModel.kt
@@ -1,30 +1,45 @@
 package com.ongo.signal.ui.main
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.asLiveData
 import androidx.lifecycle.viewModelScope
 import com.ongo.signal.config.UserSession
 import com.ongo.signal.data.model.match.MatchHistoryResponse
+import com.ongo.signal.data.model.review.ReviewRequestDTO
+import com.ongo.signal.data.model.review.ReviewResponseDTO
+import com.ongo.signal.data.model.review.ReviewResponseItemDTO
 import com.ongo.signal.data.repository.match.MatchRepository
+import com.ongo.signal.data.repository.review.ReviewRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
+import timber.log.Timber
 import javax.inject.Inject
 
 @HiltViewModel
 class ReviewViewModel @Inject constructor(
-    private val matchRepository: MatchRepository
+    private val matchRepository: MatchRepository,
+    private val reviewRepository: ReviewRepository
 ) : ViewModel() {
 
-    fun checkReviewPermission(userId: Long, onResult: (Boolean) -> Unit) {
+    private val _reviewList = MutableStateFlow<List<ReviewResponseItemDTO>>(emptyList())
+    val reviewList: StateFlow<List<ReviewResponseItemDTO>> = _reviewList
+
+    private val _isReviewVisible = MutableStateFlow(false)
+    val isReviewVisible: StateFlow<Boolean> = _isReviewVisible
+
+    fun checkReviewPermission(userId: Long) {
         getMatchHistory(userId) { historyList ->
             val matchList = historyList.filter {
                 (it.proposeId == UserSession.userId && it.acceptId == userId)
                         || (it.proposeId == userId && it.acceptId == UserSession.userId)
             }
-            if (matchList.isNotEmpty()) {
-                onResult(true)
-            } else {
-                onResult(false)
-            }
+            _isReviewVisible.value = matchList.isNotEmpty()
+            Timber.tag("userId").d(userId.toString())
+            Timber.tag("UserSessionId").d(UserSession.userId.toString())
+            Timber.d(matchList.toString())
         }
     }
 
@@ -41,5 +56,39 @@ class ReviewViewModel @Inject constructor(
         }
     }
 
+    fun loadReview(userId: Long) {
+        viewModelScope.launch {
+            runCatching {
+                reviewRepository.getReview(userId)
+            }.onSuccess { response ->
+                if (response.isSuccessful) {
+                    val newReviewList = response.body() ?: emptyList()
+                    _reviewList.value = newReviewList
+                    Timber.d(newReviewList.toString())
+                } else {
+                    val errorMessage = response.errorBody()?.string() ?: "Unknown error"
+                    Timber.e("Failed to load review: $errorMessage")
+                }
+            }.onFailure { e ->
+                Timber.e(e, "Failed to load review")
+            }
+        }
+    }
+
+    fun writeReview(reviewRequestDTO: ReviewRequestDTO) {
+        viewModelScope.launch {
+            runCatching {
+                reviewRepository.writeReview(reviewRequestDTO)
+            }.onSuccess { response ->
+                if (response.isSuccessful) {
+                    Timber.d("write review is successful: ${response.body()}")
+                } else {
+                    Timber.e("Failed to write review: ${response.errorBody()}")
+                }
+            }.onFailure { e ->
+                Timber.e(e, "Failed to write review")
+            }
+        }
+    }
 
 }
