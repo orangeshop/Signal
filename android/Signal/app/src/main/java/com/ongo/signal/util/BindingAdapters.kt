aeb0126eaf9ca818a102d2d997f2bc929fa21fea
DESKTOP-6ABJ64L\SSAFY <cjh0630@naver.com>
2024-08-05 17:27:52 +0900

    feat: profile을 보여줄 bindingAdapter 추가

--- android/Signal/app/src/main/java/com/ongo/signal/util/BindingAdapters.kt
+++ android/Signal/app/src/main/java/com/ongo/signal/util/BindingAdapters.kt
@@ -2,6 +2,7 @@ package com.ongo.signal.util
 
 import android.net.Uri
 import android.widget.ImageView
+import android.widget.TextView
 import androidx.databinding.BindingAdapter
 import com.bumptech.glide.Glide
 import com.bumptech.glide.request.RequestOptions
@@ -27,11 +28,27 @@ object BindingAdapters {
     }
 
     @JvmStatic
+    @BindingAdapter("profile")
+    fun loadProfile(view: ImageView, url: String?) {
+        Glide.with(view.context)
+            .load(url)
+            .circleCrop()
+            .into(view)
+    }
+
+    @JvmStatic
     @BindingAdapter("imageUrls")
     fun loadImages(view: ImageView, urls: List<String>?) {
         val url = urls?.firstOrNull()
         Glide.with(view.context)
             .load(url)
             .into(view)
+    }
+
+    @JvmStatic
+    @BindingAdapter("listSize")
+    fun setListSize(view: TextView, list: List<*>?) {
+        val size = list?.size ?: 0
+        view.text = " ($size)"
     }
 }
