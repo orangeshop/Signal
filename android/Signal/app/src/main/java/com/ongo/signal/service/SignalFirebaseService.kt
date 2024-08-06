d02cfb63a7fd535d92f07711d7bdb30c068aaa6c
upsk1 <55921031+upsk1@users.noreply.github.com>
2024-08-06 15:05:48 +0900

    feat: 영상통화 , 채팅 연결

--- android/Signal/app/src/main/java/com/ongo/signal/service/SignalFirebaseService.kt
+++ android/Signal/app/src/main/java/com/ongo/signal/service/SignalFirebaseService.kt
@@ -17,82 +17,82 @@ class SignalFirebaseService : FirebaseMessagingService() {
     }
 
     override fun onMessageReceived(remoteMessage: RemoteMessage) {
-        val messageTitle = remoteMessage.data.get("title").toString()
-        val messageContent = remoteMessage.data.get("body").toString()
-        Timber.d("메시지옴 제목 : ${messageTitle} 내용 : ${messageContent}")
-        //승낙, 거부
-
-        if (messageTitle.contains("테스트")) {
-            val mainIntent = Intent(this, MainActivity::class.java).apply {
-                flags = Intent.FLAG_ACTIVITY_SINGLE_TOP
-            }
-
-            val mainPendingIntent: PendingIntent = PendingIntent.getActivity(
-                this,
-                0,
-                mainIntent,
-                PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE
-            )
-
-            val builder1 = NotificationCompat.Builder(this, MainActivity.CHANNEL_ID)
-                .setSmallIcon(R.drawable.app_icon)
-                .setContentTitle(messageTitle)
-                .setContentText(messageContent)
-                .setAutoCancel(true)
-                .setContentIntent(mainPendingIntent)
-
-
-            val notificationManager: NotificationManager =
-                getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
-            notificationManager.notify(101, builder1.build())
-
-        } else {
-            val nowMessage = messageContent.split(" ")
-
-            val mainIntent = Intent(this, MainActivity::class.java).apply {
-                flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
-                putExtra("matchNotification", messageTitle)
-                putExtra("otherUserId", nowMessage[0].toLong())
-                putExtra("otherUserName", nowMessage[2])
-            }
-
-            val guideTitle = when (messageTitle) {
-                "요청" -> "매칭 신청"
-
-                "승낙" -> "매칭 성공"
-
-                "거부" -> "매칭 거부"
-                else -> ""
-            }
-            val guideContent = when (messageTitle) {
-                "요청" -> "${nowMessage[2]} 님께 매칭 신청이 왔습니다."
-
-                "승낙" -> "${nowMessage[2]} 님이 매칭을 수락하였습니다!"
-
-                "거부" -> "${nowMessage[2]} 님이 매칭을 거절하였습니다."
-                else -> ""
-            }
-
-            val mainPendingIntent: PendingIntent = PendingIntent.getActivity(
-                this,
-                0,
-                mainIntent,
-                PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE
-            )
-
-            val builder1 = NotificationCompat.Builder(this, MainActivity.CHANNEL_ID)
-                .setSmallIcon(R.drawable.app_icon)
-                .setContentTitle(guideTitle)
-                .setContentText(guideContent)
-                .setAutoCancel(true)
-                .setContentIntent(mainPendingIntent)
-
-            val notificationManager: NotificationManager =
-                getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
-            notificationManager.notify(101, builder1.build())
-
-
-        }
+//        val messageTitle = remoteMessage.data.get("title").toString()
+//        val messageContent = remoteMessage.data.get("body").toString()
+//        Timber.d("메시지옴 제목 : ${messageTitle} 내용 : ${messageContent}")
+//        //승낙, 거부
+//
+//        if (messageTitle.contains("테스트")) {
+//            val mainIntent = Intent(this, MainActivity::class.java).apply {
+//                flags = Intent.FLAG_ACTIVITY_SINGLE_TOP
+//            }
+//
+//            val mainPendingIntent: PendingIntent = PendingIntent.getActivity(
+//                this,
+//                0,
+//                mainIntent,
+//                PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE
+//            )
+//
+//            val builder1 = NotificationCompat.Builder(this, MainActivity.CHANNEL_ID)
+//                .setSmallIcon(R.drawable.app_icon)
+//                .setContentTitle(messageTitle)
+//                .setContentText(messageContent)
+//                .setAutoCancel(true)
+//                .setContentIntent(mainPendingIntent)
+//
+//
+//            val notificationManager: NotificationManager =
+//                getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+//            notificationManager.notify(101, builder1.build())
+//
+//        } else {
+//            val nowMessage = messageContent.split(" ")
+//
+//            val mainIntent = Intent(this, MainActivity::class.java).apply {
+//                flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+//                putExtra("matchNotification", messageTitle)
+//                putExtra("otherUserId", nowMessage[0].toLong())
+//                putExtra("otherUserName", nowMessage[2])
+//            }
+//
+//            val guideTitle = when (messageTitle) {
+//                "요청" -> "매칭 신청"
+//
+//                "승낙" -> "매칭 성공"
+//
+//                "거부" -> "매칭 거부"
+//                else -> ""
+//            }
+//            val guideContent = when (messageTitle) {
+//                "요청" -> "${nowMessage[2]} 님께 매칭 신청이 왔습니다."
+//
+//                "승낙" -> "${nowMessage[2]} 님이 매칭을 수락하였습니다!"
+//
+//                "거부" -> "${nowMessage[2]} 님이 매칭을 거절하였습니다."
+//                else -> ""
+//            }
+//
+//            val mainPendingIntent: PendingIntent = PendingIntent.getActivity(
+//                this,
+//                0,
+//                mainIntent,
+//                PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE
+//            )
+//
+//            val builder1 = NotificationCompat.Builder(this, MainActivity.CHANNEL_ID)
+//                .setSmallIcon(R.drawable.app_icon)
+//                .setContentTitle(guideTitle)
+//                .setContentText(guideContent)
+//                .setAutoCancel(true)
+//                .setContentIntent(mainPendingIntent)
+//
+//            val notificationManager: NotificationManager =
+//                getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+//            notificationManager.notify(101, builder1.build())
+//
+//
+//        }
     }
 
 }
