901144a7dbd0d422079a77b95ee6a1b1bdc3dce0
DESKTOP-6ABJ64L\SSAFY <cjh0630@naver.com>
2024-08-06 15:26:27 +0900

    chore: 로그 제거

--- android/Signal/app/src/main/java/com/ongo/signal/ui/main/adapter/CommentAdapter.kt
+++ android/Signal/app/src/main/java/com/ongo/signal/ui/main/adapter/CommentAdapter.kt
@@ -5,16 +5,13 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
-import com.ongo.signal.R
 import com.ongo.signal.data.model.main.CommentDTOItem
 import com.ongo.signal.databinding.ItemCommentBinding
-import com.ongo.signal.util.BindingAdapters
-import timber.log.Timber
 
 class CommentAdapter(
     private val onCommentEditClick: (CommentDTOItem) -> Unit,
     private val onCommentDeleteClick: (CommentDTOItem) -> Unit,
-    private val currentUserId: Int
+    private val currentUserId: Long
 ) :
     ListAdapter<CommentDTOItem, CommentAdapter.ViewHolder>(DiffUtilCallback()) {
 
@@ -26,8 +23,6 @@ class CommentAdapter(
             binding.tvEditComment.setOnClickListener { onCommentEditClick(comment) }
             binding.tvDeleteComment.setOnClickListener { onCommentDeleteClick(comment) }
             binding.executePendingBindings()
-
-            Timber.d("Binding comment with ID: ${comment.id}, URL: ${comment.url}")
         }
     }
 
