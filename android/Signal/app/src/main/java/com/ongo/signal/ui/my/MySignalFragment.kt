07162049b3210b8f0384a0567d4c990d3f393ced
DESKTOP-6ABJ64L\SSAFY <cjh0630@naver.com>
2024-08-06 17:04:02 +0900

    feat: viewModel 추가 및 onclick 메소드 적용

--- android/Signal/app/src/main/java/com/ongo/signal/ui/my/MySignalFragment.kt
+++ android/Signal/app/src/main/java/com/ongo/signal/ui/my/MySignalFragment.kt
@@ -4,15 +4,22 @@ import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.findNavController
 import com.ongo.signal.R
 import com.ongo.signal.config.BaseFragment
+import com.ongo.signal.config.UserSession
 import com.ongo.signal.databinding.FragmentMySignalBinding
 import com.ongo.signal.ui.MainActivity
+import com.ongo.signal.ui.main.viewmodel.BoardViewModel
 import com.ongo.signal.ui.my.adapter.PreviewPostAdapter
 import timber.log.Timber
 
 class MySignalFragment : BaseFragment<FragmentMySignalBinding>(R.layout.fragment_my_signal) {
 
-    private val previewPostAdapter = PreviewPostAdapter(onClick = {})
-    private val viewModel: MyPageViewModel by activityViewModels()
+    private val myPageViewModel: MyPageViewModel by activityViewModels()
+    private val boardViewModel: BoardViewModel by activityViewModels()
+
+    private val previewPostAdapter = PreviewPostAdapter(onClick = {
+        boardViewModel.selectBoard(it)
+        findNavController().navigate(R.id.action_mySignalFragment_to_postFragment)
+    })
 
     override fun init() {
         loadData()
@@ -24,12 +31,16 @@ class MySignalFragment : BaseFragment<FragmentMySignalBinding>(R.layout.fragment
     }
 
     private fun loadData() {
-        val userId = 3L
-        viewModel.getMySignal(userId, onSuccess = { posts ->
-            previewPostAdapter.submitList(posts)
-        }, onError = { error ->
-            Timber.e("Failed to load signals: ${error.message}")
-        })
+        val userId = UserSession.userId
+
+        userId?.let {
+            Timber.d(userId.toString())
+            myPageViewModel.getMySignal(userId, onSuccess = { posts ->
+                previewPostAdapter.submitList(posts)
+            }, onError = { error ->
+                Timber.e("Failed to load signals: ${error.message}")
+            })
+        }
     }
 
     override fun onDestroyView() {
